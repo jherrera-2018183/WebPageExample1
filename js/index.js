f2d7abfd39afbb4ccb379b3c5ce85569feabc294
Jose Herrera <joseherrera-2018183@kinal.edu.gt>
2023-01-20 08:58:51 -0600

    primer commit v1 webpage

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function saludar(){
+
+    let mumA = document.getElementById('valorA').value;
+    let numB = document,getElementById('valorB').value;
+
+    let resultado = parseInt(numA) + parseInt(numB);
+    
+    console.log('redultado')
+}
